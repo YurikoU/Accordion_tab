375bdacfea9f789c0c6288579e7ebbd64bd41446
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-06-29 19:18:14 -0700

    Add files via upload

--- accordion.js
+++ accordion.js
@@ -1,72 +1,62 @@
 (() => {
 
-    //Declare the class
     class Accordion {
 
-        constructor(obj){
-
-            console.log(obj.hookName, obj.tagName);
-
+        constructor(obj) {
             //Declare the variables
-            const $elm = document.querySelector(obj.hookName);
-            const $trigger = $elm.getElementsByTagName(obj.tagName);
-            const triggerLen = $trigger.length;
-        
+            const $doc = document;
+            const $tagContainer = $doc.getElementById(obj.containerName);
+            const $tag = $tagContainer.getElementsByTagName(obj.tagName);
+            const tagLen = $tag.length;
+
             //clickHandler() works, once any DOM element is clicked
             let index = 0;
-            while (index < triggerLen) {
-                $trigger[index].addEventListener
-                ('click', (e) => this.clickHandler(e));
+            while (index < tagLen) {
+                $tag[index].addEventListener('click', (e) => this.clickHandler(e));
                 index++;
             }
-        }
-        
+        };
 
 
         //Movement when a DOM element is clicked
-        clickHandler(e) {
-          //Prevent the anchor link
-          e.preventDefault();
+        clickHandler = (e) => {
+            //Prevent the anchor link
+            e.preventDefault();
 
-          //Get the clicked DOM element
-          const $target = e.currentTarget;
+            //Get the clicked DOM element
+            const target = e.target;
 
-          //Get the DOM element next to the DOM element
-          const $content = $target.nextElementSibling;
+            //Get the DOM element next to the DOM element
+            const targetContent = target.nextElementSibling;
 
-          //If the DOM element is visible, make it NOT visible. Otherwise, make it visible.
-          if ($content.style.display === 'block') {
-              $content.style.display = 'none';
-          } else {
-              $content.style.display = 'block';
-          }
+            //If the DOM element is visible, make it NOT visible. Otherwise, make it visible.
+            if (targetContent.style.display === 'none') {
+                targetContent.style.display = 'block';
+            } else {
+                targetContent.style.display = 'none';
+            }
         };
-    }
+    } //End of the class, Accordion{}
 
 
 
+    //Instantiate Accordion, and name it
+    const accordion1 = new Accordion({
+        containerName: 'js-faq',
+        tagName: 'p'
+    });
 
-    //Instantiate Accordion(), and name it
-    const dummyAccordion1 = new Accordion({
-        hookName: '#js-faq',
-        tagName: 'p' 
+    //Instantiate Accordion, and name it
+    const accordion2 = new Accordion({
+        containerName: 'js-accordion',
+        tagName: 'a'
+    });
+
+    //Instantiate Accordion, and name it
+    const accordionMini = new Accordion({
+        containerName: 'js-accordion-mini',
+        tagName: 'dt'
     });
 
 
-    //Instantiate Accordion(), and name it
-    const dummyAccordion2 = new Accordion({
-      hookName: '#js-accordion',
-      tagName: 'a' 
-    });
-
-
-    //Instantiate Accordion(), and name it
-    const miniAccordion = new Accordion({
-      hookName: '#js-accordion-mini',
-      tagName: 'dt' 
-    });
-
-
-
-    
 })();
